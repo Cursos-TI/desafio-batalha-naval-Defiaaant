1b5e93caa784def930b33db53ba179bb05daa757
Defiaaant <raileoncior41@gmail.com>
2025-04-21 13:51:39 +0000

    Nível aventureiro completo.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,12 +3,18 @@
 int main() {
 
     //Declaração da matriz
-    int tabuleiro [5] [5] = {
-        {0, 0, 0, 0, 0},
-        {0, 3, 0, 0, 0},
-        {0, 3, 0, 0, 0},
-        {0, 0, 3, 3, 3},
-        {0, 0, 0, 0, 0}
+    long int tabuleiro [10] [10] = {
+        //Ampliação da matriz para 100 casas
+        {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 3, 0, 3, 3, 3, 3, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 0, 0, 0, 0, 3, 0, 0, 0, 3},
+        {0, 0, 0, 0, 3, 0, 0, 0, 0, 3},
+        {0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0, 0, 0}
     };
     char letra  = 'A';
     int numero = 1;
@@ -17,35 +23,29 @@ int main() {
     //Início da matriz da batalha naval
     printf("    ");
     //Coordenada em x
-    while (numero < 6){
+    while (numero < 11){
+        //incremento do espaço de análise
         printf("%d ", numero);
         numero++;
     }
     printf("\n");
-    for (int i = 0; i < 5; i++){
         //Coordenada em y
+    for (int i = 0; i < 10; i++){
+        //incremento do espaço de análise
         printf(" %c| ", letra);
         ++letra;
         //Estrutura da matriz contendo os navios
-        for (int j = 0; j < 5; j++){
-            printf("%d ", tabuleiro[i][j]);
+        for (int j = 0; j < 10; j++){
+        //incremento do espaço de análise
+            printf("%ld ", tabuleiro[i][j]);
         }
         printf("\n");
         //Fim da matriz
     }
     
     /* partes contidas previamente no código
-
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
+    Instruções do nível novato e aventureiro excluídas.
+ 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
